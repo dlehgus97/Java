28e65f12bccb738bdc24e3a219177bdf8309b1ea
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-14 01:34:41 +0900

    Update 2438 별찍기 - 1.java

--- baekjoon/2438 별찍기 - 1.java
+++ baekjoon/2438 별찍기 - 1.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
- 
-public class Main{
-    public static void main(String[] args){
+
+public class Main {
+    public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        for (int i=0; i<n; i++){
-            for (int j=0; j<i; j++){
-                System.out.print("*");
+
+        int N  = sc.nextInt();
+
+        for(int i = 1; i <=N ; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*"); //println은 개행문자가 포함되어 있어 자동으로 넘어간다...
             }
-            System.out.println("*");
+            System.out.println();
         }
     }
 }
