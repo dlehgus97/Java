fc48d7ac62f424918de2d3ebf6eb84ab16691c77
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-15 03:35:17 +0900

    Update 2739 구구단 .java

--- baekjoon/2739 구구단 .java
+++ baekjoon/2739 구구단 .java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);	
-		int a = in.nextInt();
-		
-		in.close();		
-		
-		for(int i = 1; i<10;i++) {
-			System.out.println(a+" * "+i+" = "+(a*i));
-		}
-	}
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        for(int i = 1 ; i <=9; i++){
+            System.out.print(N + " * ");
+            System.out.print(i + " = ");
+            System.out.print(N*i +"\n");
+            //System.out.println(N+" * "+i+" = "+(N*i));
+        }
+
+    }
+
 }
+
+
+
+
