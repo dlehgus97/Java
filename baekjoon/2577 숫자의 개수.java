d203ebe906da15754578dd15ad88332ea5b5e955
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-15 02:58:40 +0900

    Update 2577 숫자의 개수.java

--- baekjoon/2577 숫자의 개수.java
+++ baekjoon/2577 숫자의 개수.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int value = (in.nextInt() * in.nextInt() * in.nextInt());
-		String str = Integer.toString(value);
-		in.close();
-		
-		for (int i = 0; i < 10; i++) {
-			int count = 0;
-			for (int j = 0; j < str.length(); j++) {
-				if ((str.charAt(j) - '0') == i) {
-					count++;
-				}
-			}
-			System.out.println(count);
-		}
-		
-	}
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        int [] arr = new int [10];
+        int result = a*b*c;  //int result (in.nextInt() * in.nextInt() * in.nextInt()); 애초에 이렇게 설정해도 될것 ..!
+
+        while(result!= 0){
+            arr[result%10] +=1;
+            result /=10;
+        }
+    for(int i = 0; i<10; i++){
+    System.out.println(arr[i]);
+        }
+    }
+
 }
+
+
+
+
