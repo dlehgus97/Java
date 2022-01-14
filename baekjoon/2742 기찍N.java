ebc39dfc8c9356d955454ecfede5216ca39e0f8c
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-15 03:37:17 +0900

    Update 2742 기찍N.java

--- baekjoon/2742 기찍N.java
+++ baekjoon/2742 기찍N.java
@@ -1,18 +1,13 @@
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		in.close();
-        
-		
-        
-		 for( int i =N ; i > 0 ; i-- ) {
-			System.out.println(N);
-             N--;
-		}
-	}
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i =N; i>=1; i--){
+            System.out.println(i);
+        }
+    sc.close();
+    }
 }
- 
