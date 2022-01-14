c054959c892c04e616eef53cc18d1d3cf14b013c
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-15 03:38:00 +0900

    Update 2562 최댓값.java

--- baekjoon/2562 최댓값.java
+++ baekjoon/2562 최댓값.java
@@ -1,26 +1,22 @@
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int[] arr = { in.nextInt(), in.nextInt(), in.nextInt(),
-				in.nextInt(), in.nextInt(), in.nextInt(),
-				in.nextInt(), in.nextInt(), in.nextInt() };
-		in.close();
-		
-		int count = 0;
-		int max = 0;
-		int index = 0;
-        
-		for(int value : arr) {
-			count++;
-            
-			if(value > max) {
-				max = value;
-				index = count;
-			}
-		}
-		System.out.print(max + "\n" + index);
-		
-	}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int [] arr = new int [9];
+        for(int i = 0 ; i< 9; i++){
+            arr[i] = in.nextInt();
+        }
+        int max = 0;
+        int index = 0;
+
+        for(int i = 0; i<9; i++){
+            if(arr[i]> max){
+                max = arr[i];
+                index = i+1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
 }
