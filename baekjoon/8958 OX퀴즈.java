c845d78e805f82aee9cff9b9e1fa8b9391a2526e
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-19 03:09:55 +0900

    Update 8958 OX퀴즈.java

--- baekjoon/8958 OX퀴즈.java
+++ baekjoon/8958 OX퀴즈.java
@@ -1,34 +1,73 @@
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		String arr[] = new String[in.nextInt()];
- 
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = in.next();
-		}
-		
-		in.close();
-		
-		for (int i = 0; i < arr.length; i++) {
-			
-			int cnt = 0;	
-			int sum = 0;	
-			
-			for (int j = 0; j < arr[i].length(); j++) {
-				
-				if (arr[i].charAt(j) == 'O') {
-					cnt++;
-				} 
-				else {
-					cnt = 0;
-				}
-				sum += cnt;
-			}
-			
-			System.out.println(sum);
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String s;
+        int score = 0;
+        int result = 0;
+
+
+        for(int i = 0; i< N; i++){
+            s = sc.next();
+
+            for(int j = 0; j<s.length(); j++){
+                if(s.charAt(j) =='O'){
+                    score +=1;
+                    result += score;
+                }
+
+                else{
+                    score = 0;
+                }
+            }
+               System.out.println(result);
+            result = 0;
+            score = 0;
+
+        }
+
+
+    }
 }
+
+
+/* 처음에는 score 가 마지막에 O일때 다음 score로 넘어가는것을 고려 하지 못했다... 
+그래서 result값과 같이 score 값을 0으로 맞춰줬다.
+생각해보면 차라리 첫번째 for 문에 int result = 0; int score = 0 ; 을 설정하면 
+마지막에 0으로 초기화 할 필요가 없는걸 알게 되었다 .. 
+수정하자면 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String s;
+        
+
+
+        for(int i = 0; i< N; i++){
+            	s = sc.next();
+		int score = 0;
+       		int result = 0;
+            for(int j = 0; j<s.length(); j++){
+                if(s.charAt(j) =='O'){
+                    score +=1;
+                    result += score;
+                }
+
+                else{
+                    score = 0;
+                }
+            }
+               System.out.println(result);
+           
+
+        }
+
+
+    }
+}
+
