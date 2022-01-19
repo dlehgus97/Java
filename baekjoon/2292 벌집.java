a89628689173c301636a169e72c565abc9769c69
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-20 00:00:52 +0900

    Update 2292 벌집.java

--- baekjoon/2292 벌집.java
+++ baekjoon/2292 벌집.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
- 
 public class Main {
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		int count = 1; 
-		int num = 2;	 
- 
-		if (N == 1) {
-			System.out.print(1);
-		}
- 
-		else {
-			while (num <= N) {	
-				num = num + (6 * count);	 
-				count++;	
-			}
-			System.out.print(count);
-		}
-	}
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int a = 1;
+        int b = 1;
+       while(N>a){
+           a += 6*b;
+           b+=1;
+
+       }
+        System.out.println(b);
+        }
+
+    }
+
+//벌집의 시작숫자와 마지막 숫자들이 6으로 이루어진 수열이다 . 이점만 알고 있으면 될것 같다
