35de0c8d1881e521ece0dffb5355265a84c2e0e8
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-19 22:02:06 +0900

    Update 10250 ACM호텔.java

--- baekjoon/10250 ACM호텔.java
+++ baekjoon/10250 ACM호텔.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
- 
-public class Main {
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
- 
-		int T = in.nextInt();	
+import java.util.Arrays;
 
-		for(int i = 0; i < T; i++) {
-			
-			int H = in.nextInt();
-			int W = in.nextInt(); 	
-			int N = in.nextInt();
-			
-			if(N % H == 0) {
-				System.out.println((H * 100) + (N / H));
- 
-			} else {
-				System.out.println(((N % H) * 100) + ((N / H) + 1));
-			}
-		}
-	}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int i = 0 ; i<T; i++){
+            int H = sc.nextInt();
+            int W = sc.nextInt();
+            int N = sc.nextInt();
+
+           
+            int Floor = N % H;
+            int Hosu = (N/H) +1;
+
+            if(Floor == 0){
+                Floor = H;
+                Hosu = N/H;
+            }
+
+            int sum = (Floor*100) + Hosu;
+
+
+
+            System.out.println(sum);
+        }
+    }
 }
+
+
+// 나머지를 이용한 계산이기때문에 나머지가 없을때도 계산 해주어야 한다 .
