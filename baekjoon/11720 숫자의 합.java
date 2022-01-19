e430063bac6efbced1bda50169fe819f0a8c6683
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-19 15:31:26 +0900

    Update 11720 숫자의 합.java

--- baekjoon/11720 숫자의 합.java
+++ baekjoon/11720 숫자의 합.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String[] args) {		
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		String a = in.next();
-		in.close();
-		
-		int sum = 0;
-        
-		for(int i = 0; i < N; i++) {
-			sum += a.charAt(i)-'0';
-		}
-		System.out.print(sum);
-	}
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int N = sc.nextInt();
+       String S = sc.next();
+        int result = 0 ;
+       for(int i = 0; i<N; i++){
+           result += S.charAt(i) - 48;// result += S.charAt(i) - '0';
+       }
+       System.out.println(result);
+
+
+    }
 }
+
+
+
+
+//charAt() 은 해당 문자의 아스키코드 값을 반환하므로 반드시 -48 또는 -'0'을 해줘야 정수값이 나온다 .
