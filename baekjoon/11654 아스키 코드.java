506ae31009b5b5a16449088e2c50ed03ca8919b0
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-19 15:23:11 +0900

    Create 11654 아스키 코드.java

---
+++ baekjoon/11654 아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       String S = sc.next();
+
+       int result = S.charAt(0);
+       System.out.println(result);
+
+    }
+}
+
+
