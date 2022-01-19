086c2fe134ccec200183ed93a6ee8930e8f05039
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-19 15:43:43 +0900

    Update 1085 직사각형에서 탈출.java

--- baekjoon/1085 직사각형에서 탈출.java
+++ baekjoon/1085 직사각형에서 탈출.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
- 
+import java.util.Arrays;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-        
-		int x = in.nextInt();
-		int y = in.nextInt();
-		int w = in.nextInt();
-		int h = in.nextInt();
-        
-		int x_min = Math.min(x, w-x);	
-		int y_min = Math.min(y, h-y);	
-        
-		// x와 y축 최소거리 중 가장 작은 값
-		System.out.println(Math.min(x_min, y_min));
-	}
- 
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int x = sc.nextInt();
+       int y = sc.nextInt();
+       int w = sc.nextInt();
+       int h = sc.nextInt();
+       int result_1 = (Math.min(x, y));
+       int result_2 = (Math.min(w-x, h-y));
+       System.out.println((Math.min(result_1,result_2)));
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
+
+
