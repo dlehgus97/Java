1ca97eef7181561886a887f0a1f865812b917596
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-23 23:51:56 +0900

    Create 11719 그대로 출력하기 2.java

---
+++ baekjoon/11719 그대로 출력하기 2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()){
+            String s = sc.nextLine();
+            System.out.println(s);
+
+        }
+        sc.close();
+
+
+    }
+
+
+}
+//그대로 출력하기1과 똑같다 .
