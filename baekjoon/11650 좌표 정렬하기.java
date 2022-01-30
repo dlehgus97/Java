d16b8b28944a0a6a2257aa36c89f639b8630ee94
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-01-31 00:28:02 +0900

    Update 11650 좌표 정렬하기.java

--- baekjoon/11650 좌표 정렬하기.java
+++ baekjoon/11650 좌표 정렬하기.java
@@ -1,30 +1,81 @@
-import java.util.Scanner;
-import java.util.Arrays;
- 
+// 방법 1
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
-		int[][] arr = new int[N][2];
-		
-		for(int i = 0; i < N; i++) {
-			arr[i][0] = in.nextInt();
-			arr[i][1] = in.nextInt();
-		}
-		
-		Arrays.sort(arr, (e1, e2) -> {
-			if(e1[0] == e2[0]) {
-				return e1[1] - e2[1];
-			} else {
-				return e1[0] - e2[0];
-			}
-		});
-		
-		for(int i = 0; i < N; i++) {
-			System.out.println(arr[i][0] + " " + arr[i][1]);
-		}
-	}
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int arr[][] = new int[N][2];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt((st.nextToken()));
+            arr[i][1] = Integer.parseInt((st.nextToken()));
+
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return Integer.compare(o1[1], o2[1]);
+            } else {
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+
+        }
+    }
+
 }
+
+//방법 2
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int Num = 10001;
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int arr[][] = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return Integer.compare(o1[1], o2[1]);
+            } else {
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+
+        }
+    }
+
+}
+
+/* Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return Integer.compare(o1[1], o2[1]);
+            } else {
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+	
+	
+다차원 배열에서 sort를 해주고 싶을때는 이렇게 해야한다. 이 문제의 조건인 앞에수를 먼저 비교 하고 조건문을 통해 앞의수가 같으면 뒤의 수를 비교해준다 .
