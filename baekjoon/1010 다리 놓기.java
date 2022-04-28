11055d15d95fa4e136f079f72d06c8ccd80290f9
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-04-28 17:52:18 +0900

    Update 1010 다리 놓기.java

--- baekjoon/1010 다리 놓기.java
+++ baekjoon/1010 다리 놓기.java
@@ -1,32 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
 
-        for (int i = 0; i < T; i++) {
-            int result_1 = 1;
-            int result_2 = 1;
-            int N = sc.nextInt();
-            int M = sc.nextInt();
-            int total = 0;
+        for (long i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long result = 1;
+            long N = Long.parseLong(st.nextToken());
+            long M = Long.parseLong(st.nextToken());
 
-            for (int k = M; k > M - N; k--) {
-                result_1 *= k;
+            for (long k = 0; k < N; k++) {
+                result *= M-k;
+                result/= k+1;
             }
-            for (int s = N; s > 0; s--) {
-                result_2 *= s;
-            }
-            total = result_1 /result_2;
-            System.out.println(total);
+            System.out.println(result);
+
         }
     }
-
-
 }
-
-//처음에 이렇게 풀었는데 long 으로 했을때 오버플로우가 발생할 수 있다.(long의 범위를 넘어서)
-//BigInteger 를 사용해서 내일 풀어볼것 .!
-
