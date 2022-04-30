6423247c87f23361fabd4c7021bd9f8b4207eb5f
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-04-30 16:29:39 +0900

    Update 1476 날짜 계산.java

--- baekjoon/1476 날짜 계산.java
+++ baekjoon/1476 날짜 계산.java
@@ -1,16 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int e = scan.nextInt();
-                int s = scan.nextInt();
-                int m = scan.nextInt();
-		
-		int year = 0;
-		while(true) {
-			year++;
-			if((year-e)%15 == 0 && (year-s)%28 == 0 && (year-m)%19 == 0) break;
-		}
-		System.out.print(year);
-	}
+    public static void main(String[]args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int E = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int year = 1;
+
+        while(true){
+            if(a == E && b == S && c == M){
+                System.out.println(year);
+                break;
+            }
+            a++; b++; c++;
+
+            if(a == 16){
+                a = 1;
+            }
+            if(b == 29){
+                b = 1;
+            }
+            if(c == 20){
+                c = 1;
+            }
+            year ++;
+        }
+    }
 }
