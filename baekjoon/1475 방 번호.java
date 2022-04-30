7ecd252ea0b0316c6e5e701b5fd14bb898324ad1
dlehgus97 <76528931+dlehgus97@users.noreply.github.com>
2022-04-30 16:59:42 +0900

    Update 1475 방 번호.java

--- baekjoon/1475 방 번호.java
+++ baekjoon/1475 방 번호.java
@@ -1,27 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-
-		int X = sc.nextInt();
-		int stick = 64;
-		int count = 0;
-
-		while (X > 0) {
-			if (stick > X) {
-				stick /= 2;
-
-			} else {
-				X -= stick;
-				count++;
-			}
-		}
-
-		System.out.println(count);
-		
-	}
+    public static void main(String[]args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int arr[] = new int [10];//0~9
+        String str = br.readLine();
+        int result = 0;
+        for(int i = 0 ; i<str.length(); i++){
+            arr[str.charAt(i)-'0']++;
+        }
+        int sum = arr[6] + arr[9];
+        if(sum%2 == 0 ){
+            sum /= 2 ;
+        }
+        else{
+            sum = (sum/2)+1;
+        }
+        arr[6] = arr[9] = sum;
+        for(int i = 0 ; i<str.length(); i++){
+            result = Math.max(result, arr[str.charAt(i) - '0']);
+        }
+        System.out.println(result);
+    }
 
 }
